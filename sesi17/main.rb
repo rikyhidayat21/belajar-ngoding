688bf67fa7c2098388aaba8812a9b3db0966522b
rikyhidayat21 <riky.hidayat@mekari.com>
2024-01-16 07:37:03 +0700

    sesi 17 part 1

---
+++ sesi17/main.rb
@@ -0,0 +1,17 @@
+# nama = "luffy"
+# if nama == "zoro"
+#   nama = "zorozuru"
+#   puts "Halo, zorozuru"
+# else
+#   nama = "luffytaro"
+#   puts "Halo, luffytaro"
+# end
+# puts "Selamat datang di program belajar ngoding, #{nama}"
+
+print "masukkan angka: "
+angka = gets.chomp.to_i
+if angka % 2 == 0
+  puts "genap nih nilainya"
+else
+  puts "ganjil nih nilainya"
+end
